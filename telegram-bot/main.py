32de42602e3cd9ed7856fc3bb57731cccdcbc87b
Ismail Mohammed <ismailmo4@gmail.com>
2023-09-20 14:03:17 +0100

    add naive predictor

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python
-# pylint: disable=unused-argument, import-error
-# This program is dedicated to the public domain under the CC0 license.
 
 """
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
+Bot to identify food pics from Telegram messages.
 
 Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Send food pic to bot, tries to identify food and reply with predictions.
 """
 
+import asyncio
 import logging
+from typing import List
 from dotenv import load_dotenv
 from os import getenv
 
-from telegram import ForceReply, Update
+import random
+
+from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -27,6 +23,8 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 load_dotenv()
 TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN")
@@ -40,40 +38,79 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("send me food pics")
+
+
+def sizeof_fmt(num) -> str:
+    """return human readable file size
+
+    e.g.
+
+    ```
+    sizeof_fmt(168963795964)
+    >>> '157.4GB'
+    ```
+    Args:
+        num (int): size of file in bytes
+
+    Returns:
+        str: human readable file size
+    """
+    # from Fred Cirera
+    # https://web.archive.org/web/20111010015624/http://blogmag.net/blog/read/38/Print_human_readable_file_size
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
+        if num < 1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+
+
+async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """download image and reply with prediction label"""
+    logger.info("photo received")
+    # largest photo is last index
+    file_id = update.message.photo[-1].file_id
+    new_file = await context.bot.get_file(file_id)
+    await new_file.download_to_drive()
+    logger.info(f"downloaded file of size {sizeof_fmt(new_file.file_size)}")
+
+    await update.message.reply_text(f"hang on, let me figure out what this is...")
+    await asyncio.sleep(10)
+
+    predicted_food = predict_food(file_id)
+    await update.message.reply_text(
+        f"Oh nice! I can see you have a nice meal of {predicted_food}"
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+def predict_food(image_url: str) -> str:
+    """
+    return a prediction of a food label from an image url
+
+    Args:
+        image_url (str): absolute url to image to run prediction on
+
+    Returns:
+        str: predicted food
+    """
+    logger.info(f"predicting food label for {image_url}")
+    # worst model ever lmao (can only get better from here?)
+    foods = ["chicken", "rice", "veg", "nothing"]
+    return random.choice(foods)
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+def add_handlers(application: Application) -> None:
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(MessageHandler(filters.PHOTO, photo_handler))
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+    # build application
     application = Application.builder().token(TELEGRAM_TOKEN).build()
+    add_handlers(application)
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
+    # start bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
